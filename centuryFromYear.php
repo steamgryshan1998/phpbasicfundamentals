e91a2969185cf98cc455c5bcbf6f1710f02eb99d
Yevhen Hryshko <steamgryshan1998@gmail.com>
2019-11-04 23:47:44 +0200

    Create centuryFromYear.php

---
+++ centuryFromYear.php
@@ -0,0 +1,17 @@
+<?php
+function centuryFromYear($year) {
+/*   if($year / 1000 <= 0.1)
+{
+$century = 1;
+}   else
+{
+if($year % 100 == 0)  // symbol '%' means reminder of division
+{
+$century = $year / 100;
+} else {
+$century = ($year / 100) - (($year % 100)/100) + 1;
+}
+} */
+return $century = ceil($year / 100);   //ceil - rounds a fraction upward
+}
+?>
