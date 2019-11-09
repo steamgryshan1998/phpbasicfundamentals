9dbcde45d524d94ada31802232202e3b5b63b6e7
Yevhen Hryshko <steamgryshan1998@gmail.com>
2019-11-09 18:16:02 +0200

    Create makeArrayConsecutive2.php

---
+++ makeArrayConsecutive2.php
@@ -0,0 +1,14 @@
+<?php
+function makeArrayConsecutive2($statues) {
+    $am = 0;
+    sort($statues);
+    for($i = 0; $i < count($statues); $i++)
+    {
+        if($statues[$i+1] - $statues[$i] > 1)
+        {
+            $am= $am + ($statues[$i+1] - $statues[$i] - 1);
+        }
+    }
+    return $am;
+}
+?>
