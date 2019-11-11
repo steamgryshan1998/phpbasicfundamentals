32c6168a81ece00ceaeab920bcf700c29e593482
Yevhen Hryshko <steamgryshan1998@gmail.com>
2019-11-11 23:11:38 +0200

    Create matrixElementsSum.php

---
+++ matrixElementsSum.php
@@ -0,0 +1,21 @@
+<?php
+function matrixElementsSum($matrix) {
+    $sum = 0;
+    for($i = 0, $counti = count($matrix); $i < $counti; $i++)
+    {
+        for($j = 0, $countj = count($matrix[$i]); $j < $countj; $j++)
+        {
+            if($matrix[$i][$j] == 0)
+            {
+                for ($n = $i; $n < $counti; $n++)
+                {
+                    $matrix[$n][$j] = 0;
+                    echo $matrix[$n][$j];
+                }
+            }
+            $sum+=$matrix[$i][$j];
+        }
+    }
+    return $sum;
+}
+?>
