88cb0d889ee793ba1a4431af6f2bc7fb75949440
Yevhen Hryshko <steamgryshan1998@gmail.com>
2019-11-13 23:56:16 +0200

    Create almostIncreasingSequence.php

---
+++ almostIncreasingSequence.php
@@ -0,0 +1,22 @@
+<?php
+function almostIncreasingSequence($sequence) {
+    $am = 0;
+
+    for($i = 0; $i < count($sequence)-1; $i++) {
+        if($sequence[$i] >= $sequence[$i+1]) {
+            if(count($sequence) > $i+2 &&
+                $sequence[$i] >= $sequence[$i+2] &&
+                $sequence[$i-1] >= $sequence[$i+1]) {
+                return false;
+            }
+            else
+                $am++;
+        }
+
+        if($am > 1)
+            return false;
+    }
+
+    return true;
+}
+?>
