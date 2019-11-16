c1f6cda44be2281bca2128b40ead792ae05013f9
Yevhen Hryshko <steamgryshan1998@gmail.com>
2019-11-16 17:17:51 +0200

    Create mumbling.php

---
+++ mumbling.php
@@ -0,0 +1,22 @@
+<?php
+function accum($s) {
+    str_split($s);
+    $exit = "";
+    for($i = 0; $i < strlen($s); $i++)
+    {
+        $word = "";
+        for($j = 0; $j <= $i; $j++)
+        {
+            $word .= $s[$i];
+        }
+        $word = strtolower($word);
+        $word = ucfirst($word);
+        $exit .= $word;
+        if($i < (strlen($s) - 1))
+        {
+            $exit.="-";
+        }
+    }
+    return $exit;
+}
+?>
