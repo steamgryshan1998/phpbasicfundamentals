0b962c339c899a46fdedaee1f16f2d01c9c6ee5b
Yevhen Hryshko <steamgryshan1998@gmail.com>
2019-11-27 19:44:50 +0200

    Update almostIncreasingSequence.php

--- almostIncreasingSequence.php
+++ almostIncreasingSequence.php
@@ -5,13 +5,13 @@
  *(task from app.codesignal.com)
  */
 function almostIncreasingSequence($sequence) {
-    $am = 0;
+    $am = 0;//set the initial value of amount elements that need to remove
     for($i = 0; $i < count($sequence)-1; $i++) {
-        if($sequence[$i] >= $sequence[$i+1]) {
-            if(count($sequence) > $i+2 &&
-                $sequence[$i] >= $sequence[$i+2] &&
-                $sequence[$i-1] >= $sequence[$i+1]) {
-                return false;
+        if($sequence[$i] >= $sequence[$i+1]) {//compare the current element of array with the next
+            if(count($sequence) > $i+2 &&            //if length of array bigger than increased by 2 counter and
+                $sequence[$i] >= $sequence[$i+2] &&  //current element bigger or equal than going through one element
+                $sequence[$i-1] >= $sequence[$i+1]) {//and if previous element bigger or equal than next element
+                return false;                        //then its impossible
             }
             else
                 $am++;
@@ -19,6 +19,5 @@ function almostIncreasingSequence($sequence) {
         if($am > 1)
             return false;
     }
-
-    return true;
+    return true;//if all verifications are passed then possible
 }
