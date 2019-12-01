f5ae2eea50cb2713941a6861067fa0ddcf97d8d9
Yevhen Hryshko <steamgryshan1998@gmail.com>
2019-12-01 23:50:05 +0200

    Create predictYourAge.php

---
+++ predictYourAge.php
@@ -0,0 +1,9 @@
+<?php
+function predictAge($age1,$age2,$age3,$age4,$age5,$age6,$age7,$age8){
+    $age = $age1*$age1 + $age2*$age2 + $age3*$age3 + $age4*$age4 + $age5*$age5
+        + $age7*$age7 + $age6*$age6 + $age8*$age8;
+    $age = sqrt($age);
+    $age /= 2;
+    $age = floor($age);
+    return $age;
+}
