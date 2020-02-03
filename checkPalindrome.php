504eeeeb5839a616bc4b70727db3d5f3edf70669
Yevhen Hryshko <steamgryshan1998@gmail.com>
2020-02-03 23:16:10 +0200

    Update checkPalindrome.php

--- checkPalindrome.php
+++ checkPalindrome.php
@@ -1,14 +1,18 @@
 <?php
+/*
+ * Function that checks the string for the palindrome
+ * (task from app.codesignal.com)
+ */
 function checkPalindrome($inputString) {
     $ar = str_split($inputString);//Converts a string to an array
     $s = count($ar);//Counts the number of elements in an array
     for($i = 0; $i < $s; $i++)
     {
-        if($ar[$i] != $ar[$s - 1 - $i])
+        if($ar[$i] != $ar[$s - 1 - $i])// checking the current element of an array with an element with the same number
+            // but a mirrored array
         {
-            return false;
+            return false;//in case of no palindrome
         }
     }
-    return true;
+    return true;// in case of palindrome
 }
-?>
