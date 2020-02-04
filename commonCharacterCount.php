e7a24cf27558884d1404b32b6a340c0c41beda0c
Yevhen Hryshko <steamgryshan1998@gmail.com>
2020-02-04 23:39:55 +0200

    Update commonCharacterCount.php

--- commonCharacterCount.php
+++ commonCharacterCount.php
@@ -1,17 +1,20 @@
 <?php
+/*
+ * Function that finds the number of common characters between two given strings
+ */
 function commonCharacterCount($s1, $s2) {
-    str_split($s1);
-    str_split($s2);
-    $count = 0;
-    for($i = 0; $i < strlen($s1); $i++)
+    str_split($s1);//converts first given string to an array
+    str_split($s2);//converts second given string to an array
+    $count = 0;//assignment to a variable(amount of common elements) of the initial value
+    for($i = 0; $i < strlen($s1); $i++)//go along the first array
     {
-        for($j = 0; $j < strlen($s2); $j++)
+        for($j = 0; $j < strlen($s2); $j++)//go along the second array
         {
-            if($s1[$i] == $s2[$j])
+            if($s1[$i] == $s2[$j])//comparison elements of arrays (iteration)
             {
-                $count++;
-                $s2[$j] = 0;
-                break;
+                $count++;// increase the number of identical elements
+                $s2[$j] = 0;//if elements are equal, equate to zero the element from the second array
+                break;//leaving the body of the inner cycle
             }
         }
     }
