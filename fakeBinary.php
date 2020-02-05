016d650ba91cf3a4d9c4cc36a5ab71673f68416f
Yevhen Hryshko <steamgryshan1998@gmail.com>
2020-02-05 21:12:24 +0200

    Update fakeBinary.php

--- fakeBinary.php
+++ fakeBinary.php
@@ -1,15 +1,18 @@
 <?php
+/*
+ * Function that replaces any digit below 5 with '0' and any digit 5 and above with '1' and returns the resulting string
+ * from '0' and '1'
+ */
 function fake_bin(string $s): string {
-    str_split($s);
-    for($i = 0; $i < strlen($s); $i++)
+    str_split($s);//converts given string to an array
+    for($i = 0; $i < strlen($s); $i++)//go along the array
     {
-        if($s[$i] >= 5)
+        if($s[$i] >= 5)// checking element comparing with 5
         {
-            $s[$i] = 1;
+            $s[$i] = 1;//if bigger or equal
         }  else  {
-            $s[$i] = 0;
+            $s[$i] = 0;// if smaller
         }
     }
     return $s;
 }
-?>
