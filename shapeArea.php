a7290fd017048f654ba8540395855869156570b1
Yevhen Hryshko <steamgryshan1998@gmail.com>
2020-02-12 19:50:13 +0200

    Update shapeArea.php

--- shapeArea.php
+++ shapeArea.php
@@ -1,4 +1,8 @@
 <?php
+/*
+ * function that find the area of a polygon for a given n.
+ * (task from app.codesignal.com)
+*/
 function shapeArea($n) {
     $sh = ($n*$n)+(($n-1)*($n-1));
     return $sh;
