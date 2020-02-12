a8d850d4a09e098d63eebce56389f96fcd7b8685
Yevhen Hryshko <steamgryshan1998@gmail.com>
2020-02-12 19:50:33 +0200

    Update countingDuplicates.php

--- countingDuplicates.php
+++ countingDuplicates.php
@@ -1,8 +1,13 @@
 <?php
+/*
+ * function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur
+ * more than once in the input string
+ * (task from www.codewars.com)
+*/
 function duplicateCount($text) {
-    $text = mb_strtolower($text);
+    $text = mb_strtolower($text);//mb_strtolower - casts a string to lowercase
     $m = 0;
-    foreach(count_chars($text, 1) as $i => $val)
+    foreach(count_chars($text, 1) as $i => $val)//iterate over the string
     {
         if ($val > 1)
         {
