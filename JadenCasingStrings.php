117889fffe7382a0b51304564caaef8aae3c5410
Yevhen Hryshko <steamgryshan1998@gmail.com>
2020-02-12 19:50:29 +0200

    Update JadenCasingStrings.php

--- JadenCasingStrings.php
+++ JadenCasingStrings.php
@@ -1,4 +1,9 @@
 <?php
+/*
+ * function that converts strings to how they would be written by Jaden Smith( he is known for almost always
+ * capitalizing every word)
+ * (task from www.codewars.com)
+*/
 function toJadenCase($string)
 {
     $str = ucwords($string);
