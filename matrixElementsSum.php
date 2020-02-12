e37bff8ef3a22cbe22ae118320115d3444dc6673
Yevhen Hryshko <steamgryshan1998@gmail.com>
2020-02-12 19:50:38 +0200

    Update matrixElementsSum.php

--- matrixElementsSum.php
+++ matrixElementsSum.php
@@ -1,4 +1,9 @@
 <?php
+/*
+ * function that returns the total sum of all rooms that are suitable for the CodeBots (ie: add up all the values that
+ * don't appear below a 0)
+ * (task from app.codesignal.com)
+*/
 function matrixElementsSum($matrix) {
     $sum = 0;
     for($i = 0, $counti = count($matrix); $i < $counti; $i++)
