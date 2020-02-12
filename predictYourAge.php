c676b15db7dfb54be96c106da0fa7855a42111e7
Yevhen Hryshko <steamgryshan1998@gmail.com>
2020-02-12 19:50:50 +0200

    Update predictYourAge.php

--- predictYourAge.php
+++ predictYourAge.php
@@ -1,4 +1,8 @@
 <?php
+/*
+ * function that predicts your age!
+ * (task from www.codewars.com)
+*/
 function predictAge($age1,$age2,$age3,$age4,$age5,$age6,$age7,$age8){
     $age = $age1*$age1 + $age2*$age2 + $age3*$age3 + $age4*$age4 + $age5*$age5
         + $age7*$age7 + $age6*$age6 + $age8*$age8;
