8f45f4f08a2c17c28c49be82fb992a402eae0b39
Yevhen Hryshko <steamgryshan1998@gmail.com>
2020-02-14 20:27:46 +0200

    Update makeArrayConsecutive2.php

--- makeArrayConsecutive2.php
+++ makeArrayConsecutive2.php
@@ -5,13 +5,13 @@
 */
 function makeArrayConsecutive2($statues) {
     $am = 0;
-    sort($statues);
+    sort($statues);//sorts array in ascending order
     for($i = 0; $i < count($statues); $i++)
     {
-        if($statues[$i+1] - $statues[$i] > 1)
+        if($statues[$i+1] - $statues[$i] > 1)//checking the difference between neighboring statues
         {
             $am= $am + ($statues[$i+1] - $statues[$i] - 1);
         }
     }
-    return $am;
+    return $am;//return the amount of additional statues needed
 }
