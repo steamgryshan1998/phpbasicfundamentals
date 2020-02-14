8dadd8cbb83ed89292de744d089c845827514058
Yevhen Hryshko <steamgryshan1998@gmail.com>
2020-02-14 20:27:53 +0200

    Update shapeArea.php

--- shapeArea.php
+++ shapeArea.php
@@ -4,6 +4,7 @@
  * (task from app.codesignal.com)
 */
 function shapeArea($n) {
-    $sh = ($n*$n)+(($n-1)*($n-1));
+    $sh = ($n*$n)+(($n-1)*($n-1));//the area of the square is equal to the sum of the square of "n" and the square of
+    // the "n" reduced by 1
     return $sh;
 }
