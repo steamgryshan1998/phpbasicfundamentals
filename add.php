6d1c0ff7a33c3edd3e5b65f4a1ee6591f4c8b5f1
Yevhen Hryshko <steamgryshan1998@gmail.com>
2020-02-14 20:27:38 +0200

    Update add.php

--- add.php
+++ add.php
@@ -3,5 +3,5 @@
  * Function that returns the sum of two numbers(task from app.codesignal.com)
  */
 function add($param1, $param2) {
-    return $param1 + $param2;// result of the function
+    return $param1 + $param2;// result of the function(sum)
 }
