e02e295e68ca1176a604565aada4a9442fcad942
Yevhen Hryshko <steamgryshan1998@gmail.com>
2020-02-15 21:18:23 +0200

    Update isLucky.php

--- isLucky.php
+++ isLucky.php
@@ -4,23 +4,22 @@
  * (task from app.codesignal.com)
 */
 function isLucky($n) {
-    $num = strval($n);
-    str_split($num);
-    $s1 = $s2 = 0;
-    for($i = 0; $i < strlen($num);$i++)
+    $num = strval($n);//returns the string value of a variable
+    $s1 = $s2 = 0;// assignment to a variables of the initial values
+    for($i = 0; $i < strlen($num);$i++)//go along the array(number of ticket)
     {
-        if ($i > (strlen($num)/2) - 1)
+        if ($i > (strlen($num)/2) - 1)//checking that a digit belongs to one of the two halves of the array
         {
-            $s2+=$num[$i];
+            $s2+=$num[$i];//increasing the sum of digits from second half
         }   else  {
-            $s1+=$num[$i];
+            $s1+=$num[$i];//increasing the sum of digits from first half
         }
     }
-    if($s1 == $s2)
+    if($s1 == $s2)//comparison the halves(lucky ticket)
     {
-        return true;
+        return true;// if ticket lucky
     }  else  {
-        return false;
+        return false;// if ticket no lucky
     }
 }
 
