c23df1715c25334e1ba2a7eac6f065ea0c4e27ad
Yevhen Hryshko <steamgryshan1998@gmail.com>
2020-02-18 21:38:17 +0200

    Update mumbling.php

--- mumbling.php
+++ mumbling.php
@@ -4,21 +4,21 @@
  * (task from www.codewars.com)
 */
 function accum($s) {
-    str_split($s);
-    $exit = "";
-    for($i = 0; $i < strlen($s); $i++)
+    str_split($s);//converts a string to an array
+    $exit = "";//assign the initial value to the string
+    for($i = 0; $i < strlen($s); $i++)//go along the array
     {
         $word = "";
         for($j = 0; $j <= $i; $j++)
         {
-            $word .= $s[$i];
+            $word .= $s[$i];//add the current element of the array to the string
         }
-        $word = strtolower($word);
-        $word = ucfirst($word);
+        $word = strtolower($word);//strtolower() converts a string to lowercase
+        $word = ucfirst($word);//ucfirst() converts the first character of a string to uppercase
         $exit .= $word;
         if($i < (strlen($s) - 1))
         {
-            $exit.="-";
+            $exit.="-";//adding the hyphen
         }
     }
     return $exit;
