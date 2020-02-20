fd33daa82991d8bf06801421c1a960f9d786972e
Yevhen Hryshko <steamgryshan1998@gmail.com>
2020-02-20 22:29:15 +0200

    Create WhoLikesIt.php

---
+++ WhoLikesIt.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * Function that calculates the value of DNA
+ * (task from www.codewars.com)
+ */
+function likes( $names ) {
+    switch(count($names)){//in dependance of value of amount elements of the array
+        case 0:
+            $str = 'no one likes this';
+            break;//finish iteration
+        case 1:
+            $str = $names[0].' likes this';//dot helps to add strings
+            break;
+        case 2:
+            $str = $names[0].' and '.$names[1]." like this";
+            break;
+        case 3:
+            $str = $names[0].', '.$names[1].' and '.$names[2]." like this";
+            break;
+        default:
+            $d = count($names) - 2;//amount of others
+            $str = $names[0].", ".$names[1]." and ".$d." others like this";
+    }
+    return $str;
+}
