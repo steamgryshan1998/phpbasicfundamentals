11c024a7ac3791b369a5638c673e7ce94235cd46
Yevhen Hryshko <steamgryshan1998@gmail.com>
2020-03-05 19:28:05 +0200

    Create add(first basic programm).php

---
+++ add(first basic programm).php
@@ -0,0 +1,7 @@
+<?php
+/*
+ * Function that returns the sum of two numbers(task from app.codesignal.com)
+ */
+function add($param1, $param2) {
+    return $param1 + $param2;// result of the function(sum)
+}
