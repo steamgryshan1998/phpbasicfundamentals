ea6c4614c50ebe857aba0445d8db0dfdf373ddb6
steamgryshan1998 <35163053+steamgryshan1998@users.noreply.github.com>
2020-03-05 19:43:02 +0200

    Delete add(first basic programm).php

--- add(first basic programm).php
+++
@@ -1,7 +0,0 @@
-<?php
-/*
- * Function that returns the sum of two numbers(task from app.codesignal.com)
- */
-function add($param1, $param2) {
-    return $param1 + $param2;// result of the function(sum)
-}
