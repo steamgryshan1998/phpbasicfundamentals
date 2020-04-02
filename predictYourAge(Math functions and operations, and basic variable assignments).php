a5c25a2549fb732db439d69045437edf1da80007
steamgryshan1998 <35163053+steamgryshan1998@users.noreply.github.com>
2020-04-02 20:04:47 +0300

    Update and rename predictYourAge.php to predictYourAge(Math functions and operations, and basic variable assignments).php

--- predictYourAge(Math functions and operations, and basic variable assignments).php
+++ predictYourAge(Math functions and operations, and basic variable assignments).php
@@ -6,8 +6,8 @@
 function predictAge($age1,$age2,$age3,$age4,$age5,$age6,$age7,$age8){
     $age = $age1*$age1 + $age2*$age2 + $age3*$age3 + $age4*$age4 + $age5*$age5
         + $age7*$age7 + $age6*$age6 + $age8*$age8;
-    $age = sqrt($age);
+    $age = sqrt($age);//sqrt() - extracts square root
     $age /= 2;
-    $age = floor($age);
+    $age = floor($age);//floor() - rounds a fraction down
     return $age;
 }
